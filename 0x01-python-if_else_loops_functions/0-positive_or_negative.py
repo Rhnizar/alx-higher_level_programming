70363b29c656e393ffad6686a52f92f5fa45b06d
Your Name <you@example.com>
2023-06-06 10:57:03 -0700

    task1 up5

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-if number is 0:
+import random
+number = random.randint(-10, 10)
+if number == 0 and True:
 	print("%d is zero" % number)
 elif number > 0 and True:
 	print("%d is positive" % number)
